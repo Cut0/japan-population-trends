1c0927ed20331a3d68198602c355bd2b7e9d50b2
cut0 <zeron9421@gmail.com>
2022-03-15 22:21:52 +0900

    fix: msw用のコメント追加

--- src/api-client/models/prefectures.ts
+++ src/api-client/models/prefectures.ts
@@ -11,6 +11,9 @@ export const getAllPrefectures = () => {
         .get<PrefecturesResponse>(GET_All_PREFECTURES_URL)
         .then((response) => {
           if (process.env.NODE_ENV === "development") {
+            /**
+             * MSWに渡すためresponse.dataに設定
+             */
             return response.data as unknown as Prefecture[];
           }
           return response.data.result;
