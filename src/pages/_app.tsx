e6c014deef8574e69c5144081a317896cc8c53b0
cut0 <zeron9421@gmail.com>
2022-03-16 20:30:21 +0900

    feat: sanitize.cssの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
+import "sanitize.css";
+import "sanitize.css/forms.css";
+import "sanitize.css/typography.css";
 import type { AppProps } from "next/app";
-
 import { initializeMock } from "../mock-api-client";
 
 if (process.env.NODE_ENV === "development") {
